34bd7bf893b7c27d6fbf0fe55036d46f97ea9182
radwanxadi <radwanxadi@gmail.com>
2022-11-04 12:02:08 -0400

    all my work

---
+++ arrays.js
@@ -0,0 +1,16 @@
+/*filter*/
+let arr1 = ["paper", "rock", "pencil", "paper"]
+let arr2 = arr1.filter(function(item){
+    return item.
+})
+
+
+/*map*/
+let arr4 = ["10", "13", "25", "8", "30", "45"]
+let arr3 = arr4.map(function (item){
+    return item + 1;
+})
+console.log(arr1);
+console.log(arr2);
+
+
---
+++ objects.js
@@ -0,0 +1,30 @@
+let student = {
+    id : "98765",
+    name : "peter",
+    marks : "0,56,91,67"
+}
+
+for (let key in student){
+    console.log
+}
+
+let aracade = {
+game : "street fighter 2",
+character : "ryu,ken,chun lee "
+
+}
+
+  {
+    title =  "training day"
+    character = alonzo
+    console.log
+    
+  }
+
+ var list = {
+    "firstname" : "kiran",
+    "lastname" : "kumar",
+    "salary" : 20000
+
+ 
+};
